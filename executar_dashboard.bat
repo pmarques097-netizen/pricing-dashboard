6ac75075741eb341c6436a8075a1ef72a0c9531c
pmarques097-netizen <pmarques097@gmail.com>
2026-06-07 12:15:40 -0300

    Delete executar_dashboard.bat

--- executar_dashboard.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Instalando/validando dependencias...
-python -m pip install -r requirements.txt
-echo.
-echo Iniciando Eirox Pricing...
-streamlit run "dashboard_pricing.py"
-pause
